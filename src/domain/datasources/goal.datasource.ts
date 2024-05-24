de569550ee648c46b5254dc1d6f234fda3e2140c
Valen <valenzalazar3@gmail.com>
2024-05-24 19:03:24 -0300

    add(newfiles) completing CRUD

--- src/domain/datasources/goal.datasource.ts
+++ src/domain/datasources/goal.datasource.ts
@@ -1,4 +1,5 @@
-import { CreateGoalDto } from "../dtos";
+import { CreateGoalDto, UpdateGoalDto } from "../dtos";
+
 import { GoalEntity } from "../entities/goal.entity";
 
 
@@ -9,4 +10,10 @@ export abstract class GoalDatasource {
     abstract create(createGoalDto: CreateGoalDto): Promise<GoalEntity>
 
     abstract getAllGoals(): Promise<GoalEntity[]>
+
+    abstract findGoalById(id: string): Promise<GoalEntity>
+
+    abstract updateGoal(id: string, updateGoalDto: UpdateGoalDto): Promise<GoalEntity>
+
+    abstract deleteGoal(id: string): Promise<GoalEntity>
 }
