82dcc8b90b923aaabe57459095af1642a58e185b
Valen <valenzalazar3@gmail.com>
2024-05-24 19:03:42 -0300

    add(newfiles) completing CRUD

--- src/infrastructure/datasource/goal.datasource.impl.ts
+++ src/infrastructure/datasource/goal.datasource.impl.ts
@@ -1,19 +1,40 @@
 import { prisma } from "../../data/postgres";
-import { CreateGoalDto, GoalDatasource, GoalEntity } from "../../domain";
-
-
-
-
+import { CreateGoalDto, GoalDatasource, GoalEntity, UpdateGoalDto } from "../../domain";
 
 export class GoalDatasourceImpl extends GoalDatasource {
+
     async create(createGoalDto: CreateGoalDto): Promise<GoalEntity> {
-        const newGoal = await prisma.goal.create({ data: createGoalDto })
-
-        return GoalEntity.fromObject(newGoal)
+        const newGoal = await prisma.goal.create({ data: createGoalDto });
+        return GoalEntity.fromObject(newGoal);
     }
+
     async getAllGoals(): Promise<GoalEntity[]> {
-        const goal = await prisma.goal.findMany()
-        return goal.map(goal => GoalEntity.fromObject(goal))
+        const goals = await prisma.goal.findMany();
+        return goals.map(goal => GoalEntity.fromObject(goal));
     }
 
+    async findGoalById(id: string): Promise<GoalEntity> {
+        const goal = await prisma.goal.findFirst({ where: { id } });
+
+        if (!goal) throw `Goal with id ${id} not found`;
+
+        return GoalEntity.fromObject(goal);
+    }
+
+    async updateGoal(id: string, updateGoalDto: UpdateGoalDto): Promise<GoalEntity> {
+        const updatedGoal = await prisma.goal.update({
+            where: { id },
+            data: updateGoalDto.values,
+        });
+
+        return GoalEntity.fromObject(updatedGoal);
+    }
+
+    async deleteGoal(id: string): Promise<GoalEntity> {
+        const deletedGoal = await prisma.goal.delete({
+            where: { id },
+        });
+
+        return GoalEntity.fromObject(deletedGoal);
+    }
 }
