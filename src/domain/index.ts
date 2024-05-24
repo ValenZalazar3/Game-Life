e2b0a8785736d219135af0cae776ec4a13ba7126
Valen <valenzalazar3@gmail.com>
2024-05-24 19:03:21 -0300

    add(newfiles) completing CRUD

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,6 +1,13 @@
 export * from './datasources/goal.datasource';
+
 export * from './dtos';
+
 export * from './entities/goal.entity';
+
 export * from './repositories/goal.repository';
+
 export * from './use-cases/get-goals';
 export * from './use-cases/create-goal';
+export * from './use-cases/update-goal';
+export * from './use-cases/delete-goal';
+export * from './use-cases/get-id-goal';
