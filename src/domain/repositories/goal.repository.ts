b5d6650d2120ad3d93374b6699b77c61fbb6e3bf
Valen <valenzalazar3@gmail.com>
2024-05-24 19:03:34 -0300

    add(newfiles) completing CRUD

--- src/domain/repositories/goal.repository.ts
+++ src/domain/repositories/goal.repository.ts
@@ -1,4 +1,4 @@
-import { CreateGoalDto } from "../dtos";
+import { CreateGoalDto, UpdateGoalDto } from "../dtos";
 import { GoalEntity } from "../entities/goal.entity";
 
 
@@ -7,7 +7,13 @@ import { GoalEntity } from "../entities/goal.entity";
 
 export abstract class GoalRepository {
 
-
     abstract getAllGoals(): Promise<GoalEntity[]>
+
     abstract create(createGoalDto: CreateGoalDto): Promise<GoalEntity>
+
+    abstract findGoalById(id: string): Promise<GoalEntity>
+
+    abstract updateGoal(id: string, updateGoalDto: UpdateGoalDto): Promise<GoalEntity>
+
+    abstract deleteGoal(id: string): Promise<GoalEntity>
 }
