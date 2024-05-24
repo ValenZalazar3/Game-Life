a6cdc36a1b9c318606cb846d312780dec14804d4
Valen <valenzalazar3@gmail.com>
2024-05-24 19:03:44 -0300

    add(newfiles) completing CRUD

--- src/infrastructure/repositories/goal.repository.impl.ts
+++ src/infrastructure/repositories/goal.repository.impl.ts
@@ -1,4 +1,4 @@
-import { CreateGoalDto, GoalDatasource, GoalEntity, GoalRepository } from "../../domain";
+import { CreateGoalDto, GoalDatasource, GoalEntity, GoalRepository, UpdateGoalDto } from "../../domain";
 
 
 
@@ -13,8 +13,19 @@ export class GoalRepositoryImpl extends GoalRepository {
     getAllGoals(): Promise<GoalEntity[]> {
         return this.datasource.getAllGoals()
     }
+
     create(createGoalDto: CreateGoalDto): Promise<GoalEntity> {
         return this.datasource.create(createGoalDto)
+    }
+
+    findGoalById(id: string): Promise<GoalEntity> {
+        return this.datasource.findGoalById(id)
+    }
+    deleteGoal(id: string): Promise<GoalEntity> {
+        return this.datasource.deleteGoal(id)
+    }
+    updateGoal(id: string, updateGoalDto: UpdateGoalDto): Promise<GoalEntity> {
+        return this.datasource.updateGoal(id, updateGoalDto)
     }
 
 }
