88dada296718581d468e885a614ac368fbba918d
Valen <valenzalazar3@gmail.com>
2024-05-29 17:10:50 -0300

    add(routesUser): createUser done

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { GoalRouter } from './goals/routes'
+import { AuthRoutes } from './auth/routes'
 
 
 
@@ -7,6 +8,7 @@ export class AppRoutes {
     static get routes(): Router {
         const router = Router()
         router.use('/api/goal', GoalRouter.routes)
+        router.use('/api/auth', AuthRoutes.routes)
         return router
     }
 }
