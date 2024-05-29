24c0966ac58db9003ae31760006baf2ea6c63f3e
Valen <valenzalazar3@gmail.com>
2024-05-29 17:10:16 -0300

    add(routesUser): createUser done

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,13 +1,22 @@
 export * from './datasources/goal.datasource';
+export * from './datasources/user.datasource';
 
 export * from './dtos';
 
 export * from './entities/goal.entity';
 
 export * from './repositories/goal.repository';
+export * from './repositories/user.repository';
 
 export * from './use-cases/get-goals';
 export * from './use-cases/create-goal';
 export * from './use-cases/update-goal';
 export * from './use-cases/delete-goal';
 export * from './use-cases/get-id-goal';
+
+export * from './services/email.service'
+export * from './services/hash.service'
+export * from './services/jwt.service'
+
+
+export * from './errors/custom.error'
